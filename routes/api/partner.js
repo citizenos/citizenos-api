dbf2a6e90663bee218bcb4e6bf5ce1495a24778e
Mikk Andresen <mikk.andresen@gmail.com>
2018-06-15 13:50:11 +0300

    GET /api/partners/:partnerId/topics/:sourcePartnerObjectId

---
+++ routes/api/partner.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = function (app) {
+
+    var Topic = app.get('models.Topic');
+
+    /**
+     * Get Partner Topic mapping
+     *
+     * Enables Partner to map its object id to CitizenOS topic
+     */
+    app.get('/api/partners/:partnerId/topics/:sourcePartnerObjectId', function (req, res, next) {
+        var partnerId = req.params.partnerId;
+        var sourcePartnerObjectId = req.params.sourcePartnerObjectId;
+
+        Topic
+            .findOne({
+                where: {
+                    sourcePartnerId: partnerId,
+                    sourcePartnerObjectId: sourcePartnerObjectId
+                }
+            })
+            .then(function (topic) {
+                if (!topic) {
+                    return res.notFound();
+                }
+
+                return res.ok({
+                    id: topic.id,
+                    sourcePartnerObjectId: topic.sourcePartnerObjectId
+                });
+            })
+            .catch(next);
+    });
+};
---
+++ test/api/partner.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var assert = require('chai').assert;
+var request = require('supertest');
+var app = require('../../app');
+
+var shared = require('../utils/shared')(app);
+var userLib = require('./lib/user')(app);
+var topicLib = require('./topic');
+
+var Topic = app.get('models.Topic');
+var Partner = app.get('models.Partner');
+
+var _partnerTopicRead = function (agent, partnerId, sourcePartnerObjectId, expectedHttpCode, callback) {
+    var path = '/api/partners/:partnerId/topics/:sourcePartnerObjectId'
+        .replace(':partnerId', partnerId)
+        .replace(':sourcePartnerObjectId', sourcePartnerObjectId);
+
+    agent
+        .get(path)
+        .set('Content-Type', 'application/json')
+        .expect(expectedHttpCode)
+        .expect('Content-Type', /json/)
+        .end(callback);
+};
+
+var partnerTopicRead = function (agent, partnerId, sourcePartnerObjectId, callback) {
+    _partnerTopicRead(agent, partnerId, sourcePartnerObjectId, 200, callback);
+};
+
+suite('Partners', function () {
+
+    suiteSetup(function (done) {
+        shared
+            .syncDb()
+            .then(done)
+            .catch(done);
+    });
+
+    suite('Topics', function () {
+
+        suite('Read', function () {
+            var agent = request.agent(app);
+
+            var user;
+            var partner;
+            var partnerObjectId = Math.random().toString(36).substring(16);
+            var topic;
+
+            suiteSetup(function (done) {
+                userLib.createUserAndLogin(agent, null, null, null, function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    user = res;
+
+                    Partner
+                        .findOrCreate({
+                            where: {
+                                website: 'notimportant'
+                            },
+                            defaults: {
+                                website: 'notimportant',
+                                redirectUriRegexp: 'notimportant'
+                            }
+                        })
+                        .then(function (resultPartner) {
+                            partner = resultPartner[0];
+
+                            topicLib.topicCreate(agent, user.id, null, null, null, null, null, function (err, resultTopic) {
+                                    if (err) {
+                                        return done(err);
+                                    }
+
+                                    topic = resultTopic.body.data;
+
+                                    Topic
+                                        .update(
+                                            {
+                                                sourcePartnerId: partner.id,
+                                                sourcePartnerObjectId: partnerObjectId
+                                            },
+                                            {
+                                                where: {
+                                                    id: topic.id
+                                                }
+                                            }
+                                        )
+                                        .then(function () {
+                                            done();
+                                        })
+                                        .catch(done);
+                                });
+                        })
+                        .catch(done);
+                });
+            });
+
+
+            test('Success', function (done) {
+                partnerTopicRead(request.agent(app), partner.id, partnerObjectId, function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    var expectedResult = {
+                        id: topic.id,
+                        sourcePartnerObjectId: partnerObjectId
+                    };
+
+                    assert.deepEqual(res.body.data, expectedResult);
+
+                    done();
+                });
+            });
+
+            test('Fail - 404', function (done) {
+                _partnerTopicRead(request.agent(app), partner.id, 'DOESNOTEXIST', 404, done);
+            });
+
+        });
+
+    });
+
+});
