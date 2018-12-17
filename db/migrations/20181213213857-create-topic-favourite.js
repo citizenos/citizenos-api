ced03023dc6359bc434f90a9063c0e1e736f073c
ilmar <ilmar.tyrk@gmail.com>
2018-12-18 01:22:44 +0200

    fix migration file formatting

--- db/migrations/20181213213857-create-topic-favourite.js
+++ db/migrations/20181213213857-create-topic-favourite.js
@@ -1,32 +1,33 @@
 'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('TopicFavourites', {
-      topicId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        comment: 'To what Topic this Favourite belongs to.',
-        references: {
-            model: 'Topics',
-            key: 'id'
-        },
-        primaryKey: true
-      },
-      userId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        comment: 'Which User this Favourite belongs to.',
-        references: {
-            model: 'Users',
-            key: 'id'
-        },
-        primaryKey: true
-      }
-    }, {
-      timestamps: false
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('TopicFavourites');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('TopicFavourites', {
+            topicId: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                comment: 'To what Topic this Favourite belongs to.',
+                references: {
+                    model: 'Topics',
+                    key: 'id'
+                },
+                primaryKey: true
+            },
+            userId: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                comment: 'Which User this Favourite belongs to.',
+                references: {
+                    model: 'Users',
+                    key: 'id'
+                },
+                primaryKey: true
+            }
+        }, {
+            timestamps: false
+        });
+    },
+    down: (queryInterface) => {
+        return queryInterface.dropTable('TopicFavourites');
+    }
 };
