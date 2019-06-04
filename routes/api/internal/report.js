83d5f3fd7f546e9339edda1dd2e096282d518d2a
Mikk Andresen <mikk.andresen@gmail.com>
2019-06-04 18:45:36 +0300

    CSP report oneliner so that reports are easier to read and parse - https://github.com/citizenos/citizenos-fe/issues/101

--- routes/api/internal/report.js
+++ routes/api/internal/report.js
@@ -9,13 +9,32 @@ module.exports = function (app) {
 
     app.post('/api/internal/report', function (req, res) {
 
-        logger.error(
-            'CSP report',
-            {
-                headers: req.headers,
-                body: req.body
-            }
-        );
+        const cspReport = req.body['csp-report'];
+        const headers = req.headers;
+
+        if (cspReport) {
+            // Extra long one liner with useful info so that we can do easier parsing and alerting
+            logger.error(
+                'CSP report:',
+                JSON.stringify({
+                    'blocked-uri': cspReport['blocked-uri'], // IF "data", means "data" url was used. For example: data:application/javascript;charset=utf-8;base64... May be a hack, may be a bad plugin.
+                    'effective-directive': cspReport['effective-directive'], // The directive whose enforcement caused the violation.
+                    'violated-directive': cspReport['violated-directive'], // The name of the policy section that was violated.
+                    'document-uri': cspReport['document-uri'], // The URI of the document in which the violation occurred.
+                    'referrer': cspReport['referrer'], // The referrer of the document in which the violation occurred.
+                    'script-sample': cspReport['script-sample'] // The first 40 characters of the inline script, event handler, or style that caused the violation.
+                }),
+                'Headers:',
+                JSON.stringify({
+                    'user-agent': headers['user-agent'],
+                    'x-forwarded-for': headers['x-forwarded-for'],
+                    'cf-ipcountry': headers['cf-ipcountry'],
+                    'x-request-id': headers['x-request-id']
+                })
+            );
+        } else {
+            logger.error('CSP report endpoint called with invalid payload', req.body, req.headers);
+        }
 
         res.ok();
     });
---
+++ test/api/internal/report.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const request = require('supertest');
+const app = require('../../../app');
+
+const report = function (agent, headers, payload) {
+    const path = '/api/internal/report';
+
+    return new Promise(function (resolve, reject) {
+        agent
+            .post(path)
+            .set(headers)
+            .send(JSON.stringify(payload))
+            .expect(200)
+            .end(function (err, res) {
+                if (err) return reject(err);
+                return resolve(res);
+            });
+    });
+};
+
+
+suite('Internal', function () {
+
+    suite('Report', function () {
+
+        test('Success', function (done) {
+            const agent = request.agent(app);
+
+            const headers = {
+                host: 'api.citizenos.com',
+                'cf-ipcountry': 'EE',
+                'x-forwarded-for': '90.191.10.55, 172.69.138.33',
+                'cf-ray': '4da4c158a8ddb63f-TLL',
+                'x-forwarded-proto': 'https',
+                'cf-visitor': '{"scheme":"https"}',
+                origin: 'https://app.citizenos.com',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
+                'content-type': 'application/csp-report',
+                accept: '*/*',
+                referer: 'https://app.citizenos.com/id/tenislandproject',
+                'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,et;q=0.7,fi;q=0.6',
+                'cf-connecting-ip': '90.191.10.55',
+                'cdn-loop': 'cloudflare',
+                'x-request-id': '322aef4a-36da-4d91-95f2-763d42637568',
+                'x-forwarded-port': '443',
+                via: '1.1 vegur',
+                'connect-time': '0',
+                'x-request-start': '1558422885406',
+                'total-route-time': '0'
+            };
+
+            const cspReport = {
+                'csp-report': {
+                    'document-uri': 'https://app.citizenos.com/id/tenislandproject',
+                    referrer: 'https://web.unep.org/youngchampions/2019/bio/asia-pacific',
+                    'violated-directive': 'font-src',
+                    'effective-directive': 'font-src',
+                    'original-policy': 'default-src \'self\'; img-src *; script-src \'self\' https://cdn.crowdin.com https://apis.google.com https://www.google-analytics.com; style-src \'self\' https://fonts.googleapis.com \'sha256-swnxFqaff0i3bsLtJDRpMd5tZFLBnglxSxSqPSAWdIk=\'; font-src \'self\' https://fonts.gstatic.com; connect-src \'self\' https://id.citizenos.com https://api.citizenos.com https://sentry.io https://graph.microsoft.com https://www.google-analytics.com; base-uri \'self\'; object-src \'none\'; frame-src https://p.citizenos.com/ https://accounts.google.com/ https://docs.google.com; report-uri https://api.citizenos.com/api/internal/report;',
+                    disposition: 'report',
+                    'blocked-uri': 'data',
+                    'status-code': 0,
+                    'script-sample': '<script>alert("ASD");</script>'
+                }
+            };
+
+            report(agent, headers, cspReport)
+            .then(function(){
+                done();
+            })
+            .catch(done);
+        });
+
+    });
+
+});
