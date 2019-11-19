e36e5a74a9f21baf6957c7971cd6f16bb4a83ba0
Mikk Andresen <mikk.andresen@gmail.com>
2019-11-19 14:57:21 +0200

    WIP: TopicInviteUser DB migration - https://github.com/citizenos/citizenos-fe/issues/112

---
+++ db/migrations/20191119124917-create-topic-invite-user.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var _ = require('lodash');
+const LEVELS = {
+  none: 'none', // Enables to override inherited permissions.
+  read: 'read',
+  edit: 'edit',
+  admin: 'admin'
+};
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('TopicInviteUsers', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4
+      },
+      creatorId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        comment: 'User who created the invite.',
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        comment: 'User who is invited.',
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      topicId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        comment: 'Topic to which member belongs.',
+        references: {
+          model: 'Topics',
+          key: 'id'
+        },
+        primaryKey: true
+      },
+      level: {
+        type: Sequelize.ENUM,
+        values: _.values(LEVELS),
+        allowNull: false,
+        defaultValue: LEVELS.read,
+        comment: 'User membership level.'
+      }
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('TopicInviteUsers');
+  }
+};
