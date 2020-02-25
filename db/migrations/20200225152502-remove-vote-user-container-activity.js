c68e53049a01c3863ec4d22e3a160d7e486053e7
Mikk Andresen <mikk.andresen@gmail.com>
2020-02-25 18:33:57 +0200

    ESLint - https://github.com/citizenos/citizenos-fe/issues/342

--- db/migrations/20200225152502-remove-vote-user-container-activity.js
+++ db/migrations/20200225152502-remove-vote-user-container-activity.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
-    up: (queryInterface, Sequelize) => {
+    up: (queryInterface) => {
         return queryInterface.sequelize.query('DELETE FROM "Activities" WHERE data->\'object\'->>\'@type\'=\'VoteUserContainer\'')
             .then(function (res) {
                 console.log(`DELETED ${res[1].rowCount} ACTIVITIES!`);
@@ -8,7 +8,7 @@ module.exports = {
                 return Promise.resolve();
             });
     },
-    down: (queryInterface) => {
+    down: () => {
         console.log('NO UNDO FOR THIS MIGRATION!', __filename);
 
         return Promise.resolve();
