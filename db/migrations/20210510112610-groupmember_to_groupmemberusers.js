1178d4dbdc75e5f75a77fa335d40503bd6ba7a30
Mikk Andresen <mikk.andresen@gmail.com>
2021-05-10 16:35:11 +0300

    WIP. Migration to rename ENUM enum_GroupMembers_level to enum_GroupMemberUsers_level. Migration to rename TABLE GroupMembers to GroupMemberUsers - https://github.com/citizenos/citizenos-api/issues/198

--- db/migrations/20210510112610-groupmember_to_groupmemberusers.js
+++ db/migrations/20210510112610-groupmember_to_groupmemberusers.js
@@ -8,14 +8,20 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const t = await queryInterface.sequelize.transaction();
     try {
-      // Modify remove "edit" and "none" values from the "enum_GroupInviteUsers_level"
-      console.info('Modify "enum_GroupInviteUsers_level" - remove "edit" and "none" values...');
-      queryInterface.sequelize.query(`ALTER TYPE "enum_GroupInviteUsers_level" RENAME TO "enum_GroupInviteUsers_level_old"`, {transaction: t});
-      queryInterface.sequelize.query(`CREATE TYPE "enum_GroupInviteUsers_level" AS ENUM ('read', 'admin')`, {transaction: t});
-      queryInterface.sequelize.query(`ALTER TABLE "GroupInviteUsers" ALTER COLUMN "level" DROP DEFAULT`, {transaction: t}); // Temporary removal of default value so that I can alter column type to the new ENUM
-      queryInterface.sequelize.query(`ALTER TABLE "GroupInviteUsers" ALTER COLUMN "level" TYPE "enum_GroupInviteUsers_level" USING "level"::text::"enum_GroupInviteUsers_level"`, {transaction: t});
-      queryInterface.sequelize.query(`ALTER TABLE "GroupInviteUsers" ALTER COLUMN "level" SET DEFAULT 'read'::"enum_GroupInviteUsers_level"`, {transaction: t}); // Add back the default value
-      queryInterface.sequelize.query(`DROP TYPE "enum_GroupInviteUsers_level_old"`, {transaction: t});
+      console.info('Modify ENUM "enum_GroupInviteUsers_level" - remove "edit" and "none" values...');
+      await queryInterface.sequelize.query(`ALTER TYPE "enum_GroupInviteUsers_level" RENAME TO "enum_GroupInviteUsers_level_old"`, {transaction: t});
+      await queryInterface.sequelize.query(`CREATE TYPE "enum_GroupInviteUsers_level" AS ENUM ('read', 'admin')`, {transaction: t});
+      await queryInterface.sequelize.query(`ALTER TABLE "GroupInviteUsers" ALTER COLUMN "level" DROP DEFAULT`, {transaction: t}); // Temporary removal of default value so that I can alter column type to the new ENUM
+      await queryInterface.sequelize.query(`ALTER TABLE "GroupInviteUsers" ALTER COLUMN "level" TYPE "enum_GroupInviteUsers_level" USING "level"::text::"enum_GroupInviteUsers_level"`, {transaction: t});
+      await queryInterface.sequelize.query(`ALTER TABLE "GroupInviteUsers" ALTER COLUMN "level" SET DEFAULT 'read'::"enum_GroupInviteUsers_level"`, {transaction: t}); // Add back the default value
+      await queryInterface.sequelize.query(`DROP TYPE "enum_GroupInviteUsers_level_old"`, {transaction: t});
+
+      console.info('Rename ENUM "enum_GroupMembers_level" to "enum_GroupMemberUsers_level ...');
+      await queryInterface.sequelize.query(`ALTER TYPE "enum_GroupMembers_level" RENAME TO "enum_GroupMemberUsers_level"`, {transaction: t});
+
+      console.info('Rename TABLE "GroupMembers" TO "GroupMemberUsers"...');
+      await queryInterface.sequelize.query(`ALTER TABLE "GroupMembers" RENAME TO "GroupMemberUsers"`, {transaction: t});
+
       await t.commit();
     } catch (err) {
       await t.rollback();
