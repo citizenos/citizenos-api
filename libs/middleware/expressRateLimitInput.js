1028b355bc2c71033a5b9ebeb014b659ce6d3572
Mikk Andresen <mikk.andresen@gmail.com>
2021-12-08 17:23:31 +0200

    WIP: Input rate limiting middleware - https://github.com/citizenos/citizenos-api/issues/217

--- libs/middleware/expressRateLimitInput.js
+++ libs/middleware/expressRateLimitInput.js
@@ -23,8 +23,6 @@ function ExpressRateLimitInput (app) {
      * @see https://lodash.com/docs/4.17.15#get
      */
     return function expressRateLimitInput (properties, windowMs, max) {
-        // TODO: windowMs - (node:17789) TimeoutOverflowWarning: 3600000000 does not fit into a 32-bit signed integer. - indicates lib uses "setTimeout/setInterval" internally and hits its limit
-
         assert(properties && Array.isArray(properties), `Parameter "properties" is required and must be an array of dot notation strings of properties. Value: ${properties}`);
         assert(windowMs && Number.isInteger(windowMs), `Parameter "windowMs" is required and must be an integer determining the window size in milliseconds. Value: ${windowMs}`);
         assert(max && Number.isInteger(max), `Parameter "max" and must be an integer determining the maximum number of events in given window. Value: ${max}`);
@@ -32,18 +30,20 @@ function ExpressRateLimitInput (app) {
         // The widely used express-rate-limit and express-slow-down have window size (windowMs) and maximum events in given window (max).
         // rate-limiter-flexible has a concept of duration and points. In certain duration X points can be consumed before hitting limits. This idea is better in a sense that you MAY want penalize some events more than others by consuming more than 1 point.
 
+        // NOTE: Would have liked that you pass in a rate limit storage, like express-rate-limit, so that storage type definition and limits configuration was separate.
+        // BUT, when it comes to node-rate-limiter-flexible, the storage and limiter configuration itself are defined as one.
         let rateLimiter;
         if (config.rateLimit && config.rateLimit.storageType === 'redis') {
             const Redis = require('ioredis');
             const client = new Redis(config.rateLimit.storageOptions);
             rateLimiter = new RateLimiterRedis({
                 storeClient: client,
-                duration: windowMs,
+                duration: windowMs / 1000,
                 points: max
             });
         } else {
             rateLimiter = new RateLimiterMemory({
-                duration: windowMs,
+                duration: windowMs / 1000,
                 points: max
             });
         }
