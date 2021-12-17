4b30651a28c90aa2f980433823514dc76c4648a2
Mikk Andresen <mikk.andresen@gmail.com>
2021-12-17 14:45:58 +0200

    Migration for Comment.type poi - fix migration - https://github.com/citizenos/citizenos-fe/issues/329

--- db/migrations/20211217120934-comment-type-poi.js
+++ db/migrations/20211217120934-comment-type-poi.js
@@ -2,19 +2,13 @@
 
 module.exports = {
     up: async (queryInterface) => {
-        return queryInterface.sequelize.transaction(async function (t) {
-            await queryInterface.sequelize.query(
-                `ALTER TYPE "enum_Comments_type" ADD VALUE 'poi' AFTER 'pro'`,
-                {
-                    transaction: t
-                }
-            );
-        });
+        await queryInterface.sequelize.query(
+            `ALTER TYPE "enum_Comments_type" ADD VALUE 'poi' AFTER 'pro'`
+        );
     },
 
     down: async () => {
         console.log('NO ROLLBACK FOR THIS MIGRATION.');
-        console.log('IF ANY PARTS OF THE MIGRATION FAILED, THE WHOLE MIGRATION WAS ROLLED BACK.');
         console.log('IF you want to revert this. You need to decide what to do with the data that has the new Comment.type="poi".');
         console.log('Once the data has been cleaned up, that is, no "poi" is used, you need to recreate the ENUM without the "poi" because at this point PG does not support droping values from ENUM');
     }
