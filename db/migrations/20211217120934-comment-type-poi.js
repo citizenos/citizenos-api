e98c3693d4a54c5ef0d9fcd49acf16c4123c0b4a
Mikk Andresen <mikk.andresen@gmail.com>
2021-12-17 14:42:30 +0200

    Migration for Comment.type poi - rollback comment - https://github.com/citizenos/citizenos-fe/issues/329

--- db/migrations/20211217120934-comment-type-poi.js
+++ db/migrations/20211217120934-comment-type-poi.js
@@ -15,7 +15,7 @@ module.exports = {
     down: async () => {
         console.log('NO ROLLBACK FOR THIS MIGRATION.');
         console.log('IF ANY PARTS OF THE MIGRATION FAILED, THE WHOLE MIGRATION WAS ROLLED BACK.');
-        console.log('IF you want to revert this. You need to decide what to do with the data that has the new Comment.type="poi".')
-        console.log('Once the data has been cleaned up, that is, no "poi" is used, you can remove "poi" from the ENUM.')
+        console.log('IF you want to revert this. You need to decide what to do with the data that has the new Comment.type="poi".');
+        console.log('Once the data has been cleaned up, that is, no "poi" is used, you need to recreate the ENUM without the "poi" because at this point PG does not support droping values from ENUM');
     }
 };
