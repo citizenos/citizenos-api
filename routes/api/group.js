2f2ab63619446e2ce75a7036a22be76a18044b65
ilmar <ilmar.tyrk@gmail.com>
2023-02-18 22:22:01 +0200

    Return public groups from /api/users/:userId/groups

--- routes/api/group.js
+++ routes/api/group.js
@@ -327,7 +327,7 @@ module.exports = function (app) {
         return res.ok(group);
     }));
 
-    app.get('/api/users/:userId/groups/:groupId', hasPermission(GroupMemberUser.LEVELS.read, null, null), asyncMiddleware(async function (req, res) {
+    app.get('/api/users/:userId/groups/:groupId', hasPermission(GroupMemberUser.LEVELS.read, true, null), asyncMiddleware(async function (req, res) {
         const userId = req.user.userId;
         const groupId = req.params.groupId;
         const group = await _readGroup(groupId, userId);
