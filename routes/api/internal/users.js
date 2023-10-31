3433607ac9181b02159cdace7d9060a2d8c64e1a
ilmar <ilmar.tyrk@gmail.com>
2023-10-31 14:22:19 +0200

    Add try catch to user synx

--- routes/api/internal/users.js
+++ routes/api/internal/users.js
@@ -19,17 +19,21 @@ module.exports = function (app) {
         var authorData = req.body;
         console.log('EP SYNC', req.body, authorData)
         if (authorData.userId && authorData.authorID) {
-            await User
-                .update(
-                    {authorId: authorData.authorID},
-                    {
-                        where: {
-                            id: authorData.userId
-                        },
-                        limit: 1,
-                        returning: true
-                    }
-                );
+            try {
+                await User
+                    .update(
+                        { authorId: authorData.authorID },
+                        {
+                            where: {
+                                id: authorData.userId
+                            },
+                            limit: 1,
+                            returning: true
+                        }
+                    );
+            } catch (err) {
+                console.log('ERROR /api/internal/users', err);
+            }
 
             return res.ok();
         }
