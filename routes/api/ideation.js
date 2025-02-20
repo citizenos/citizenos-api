4ed8cd9860feebd614dd4b9f6f7b706270c04e10
Ilmar Türk <ilmar.tyrk@gmail.com>
2025-02-21 00:41:55 +0200

    FIX tests

--- routes/api/ideation.js
+++ routes/api/ideation.js
@@ -558,6 +558,7 @@ module.exports = function (app) {
         const description = req.body.description;
         const imageUrl = req.body.imageUrl;
         const demographics = req.body.demographics;
+        const status = req.body.status || 'draft';
 
         try {
 
@@ -592,7 +593,8 @@ module.exports = function (app) {
                         description,
                         imageUrl,
                         ideationId,
-                        demographics
+                        demographics,
+                        status
                     });
                     idea.topicId = topicId;
                     await idea.save({ transaction: t });
