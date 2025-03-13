92743eecfb93c328f20ea1ac036ecdc51b05e3f1
Ilmar Türk <ilmar.tyrk@gmail.com>
2025-03-14 01:05:25 +0200

    WIP add database migration, update model

--- db/models/UserConnection.js
+++ db/models/UserConnection.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var _ = require('lodash');
+const _ = require('lodash');
+const config = require('config');
 
 /**
  * UserConnection
@@ -47,17 +48,170 @@ module.exports = function (sequelize, DataTypes) {
                 primaryKey: true
             },
             connectionUserId: {
-                type: DataTypes.STRING,
+                type: DataTypes.TEXT,
                 allowNull: false,
-                comment: 'User id in the connected system. For Facebook their user id, for Google their user id and so on, PID for Estonian ID infra etc.'
+                comment: 'User id in the connected system (encrypted). For Facebook their user id, for Google their user id and so on, PID for Estonian ID infra etc.',
+                set: function(v) {
+                    if (!v) {
+                        this.setDataValue('connectionUserId', null);
+                        return;
+                    }
+                    this.setDataValue('_connectionUserId_raw', v);
+                    this.setDataValue('connectionUserId', v);
+                },
+                get: async function() {
+                    const value = this.getDataValue('connectionUserId');
+                    if (!value) return null;
+
+                    // If we have raw data, return it directly
+                    if (this.getDataValue('_connectionUserId_raw')) {
+                        return this.getDataValue('_connectionUserId_raw');
+                    }
+
+                    try {
+                        const passphrase = config.get('db.passphrase');
+                        // Try decoding base64 first (for old data)
+                        const [results] = await sequelize.query(
+                            'SELECT pgp_sym_decrypt(decode(:encrypted, \'base64\'), :passphrase, \'cipher-algo=aes256\')::text as decrypted',
+                            {
+                                replacements: {
+                                    encrypted: value,
+                                    passphrase: passphrase
+                                },
+                                type: sequelize.QueryTypes.SELECT
+                            }
+                        );
+                        return results?.decrypted || null;
+                    } catch (err1) {
+                        console.error('Base64 decryption failed:', err1);
+                        try {
+                            // If base64 decode fails, try direct bytea (for new data)
+                            const passphrase = config.get('db.passphrase');
+                            const [results] = await sequelize.query(
+                                'SELECT pgp_sym_decrypt(:encrypted::bytea, :passphrase, \'cipher-algo=aes256\')::text as decrypted',
+                                {
+                                    replacements: {
+                                        encrypted: value,
+                                        passphrase: passphrase
+                                    },
+                                    type: sequelize.QueryTypes.SELECT
+                                }
+                            );
+                            return results?.decrypted || null;
+                        } catch (err2) {
+                            console.error('Bytea decryption error:', err2);
+                            return null;
+                        }
+                    }
+                }
             },
             connectionData: {
-                type: DataTypes.JSONB,
+                type: DataTypes.TEXT,
                 allowNull: true,
-                comment: 'Connection specific data you want to store.'
+                comment: 'Connection specific data (encrypted).',
+                set: function(v) {
+                    if (!v) {
+                        this.setDataValue('connectionData', null);
+                        return;
+                    }
+                    this.setDataValue('_connectionData_raw', v);
+                    this.setDataValue('connectionData', JSON.stringify(v));
+                },
+                get: async function() {
+                    const value = this.getDataValue('connectionData');
+                    if (!value) return null;
+
+                    // If we have raw data, return it directly
+                    if (this.getDataValue('_connectionData_raw')) {
+                        return this.getDataValue('_connectionData_raw');
+                    }
+
+                    try {
+                        const passphrase = config.get('db.passphrase');
+                        // Try decoding base64 first (for old data)
+                        const [results] = await sequelize.query(
+                            'SELECT pgp_sym_decrypt(decode(:encrypted, \'base64\'), :passphrase, \'cipher-algo=aes256\')::text as decrypted',
+                            {
+                                replacements: {
+                                    encrypted: value,
+                                    passphrase: passphrase
+                                },
+                                type: sequelize.QueryTypes.SELECT
+                            }
+                        );
+                        return JSON.parse(results?.decrypted || null);
+                    } catch (err1) {
+                        console.error('Base64 decryption failed:', err1);
+                        try {
+                            // If base64 decode fails, try direct bytea (for new data)
+                            const passphrase = config.get('db.passphrase');
+                            const [results] = await sequelize.query(
+                                'SELECT pgp_sym_decrypt(:encrypted::bytea, :passphrase, \'cipher-algo=aes256\')::text as decrypted',
+                                {
+                                    replacements: {
+                                        encrypted: value,
+                                        passphrase: passphrase
+                                    },
+                                    type: sequelize.QueryTypes.SELECT
+                                }
+                            );
+                            return JSON.parse(results?.decrypted || null);
+                        } catch (err2) {
+                            console.error('Bytea decryption error:', err2);
+                            return null;
+                        }
+                    }
+                }
             }
         }
     );
+
+    // Add beforeCreate hook for encryption
+    UserConnection.beforeCreate(async (connection, options) => {
+        const passphrase = config.get('db.passphrase');
+
+        // Handle connectionUserId encryption
+        if (connection.getDataValue('_connectionUserId_raw')) {
+            const value = connection.getDataValue('_connectionUserId_raw');
+            const [result] = await sequelize.query(
+                'SELECT pgp_sym_encrypt(:value::text, :passphrase, \'cipher-algo=aes256\') as encrypted',
+                {
+                    replacements: {
+                        value: value,
+                        passphrase: passphrase
+                    },
+                    type: sequelize.QueryTypes.SELECT,
+                    transaction: options.transaction
+                }
+            );
+            connection.setDataValue('connectionUserId', result.encrypted);
+            connection.setDataValue('_connectionUserId_raw', null);
+        }
+
+        // Handle connectionData encryption
+        if (connection.getDataValue('_connectionData_raw')) {
+            const value = JSON.stringify(connection.getDataValue('_connectionData_raw'));
+            const [result] = await sequelize.query(
+                'SELECT pgp_sym_encrypt(:value::text, :passphrase, \'cipher-algo=aes256\') as encrypted',
+                {
+                    replacements: {
+                        value: value,
+                        passphrase: passphrase
+                    },
+                    type: sequelize.QueryTypes.SELECT,
+                    transaction: options.transaction
+                }
+            );
+            connection.setDataValue('connectionData', result.encrypted);
+            connection.setDataValue('_connectionData_raw', null);
+        }
+    });
+
+    // Add beforeUpdate hook for encryption
+    UserConnection.beforeUpdate(async (connection, options) => {
+        // Same encryption logic as beforeCreate
+        await UserConnection.beforeCreate(connection, options);
+    });
 
     UserConnection.associate = function (models) {
         UserConnection.belongsTo(models.User, {
@@ -73,6 +227,19 @@ module.exports = function (sequelize, DataTypes) {
         };
 
         return user;
+    };
+
+    // Add a method to get decrypted values
+    UserConnection.prototype.getDecryptedFields = async function() {
+        const [connectionUserId, connectionData] = await Promise.all([
+            this.connectionUserId,
+            this.connectionData
+        ]);
+        return {
+            ...this.toJSON(),
+            connectionUserId,
+            connectionData
+        };
     };
 
     UserConnection.CONNECTION_IDS = CONNECTION_IDS;
