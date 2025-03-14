0cb91993a2a5ac92d91a3daeea3a1c5fecd277f6
Ilmar Türk <ilmar.tyrk@gmail.com>
2025-03-15 01:40:55 +0200

    FIX code and tests

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -564,7 +564,7 @@ module.exports = function (app) {
         try {
             const sessionData = await smartId.authenticate(pid, countryCode);
             sessionData.userId = userId;
-            const sessionDataEncrypted = {sessionDataEncrypted: cryptoLib.privateEncrypt(config.session.secret, sessionData)};
+            const sessionDataEncrypted = {sessionDataEncrypted: cryptoLib.encrypt(config.session.secret, sessionData)};
             const token = jwt.sign(sessionDataEncrypted, config.session.privateKey, {
                 expiresIn: '5m',
                 algorithm: config.session.algorithm
@@ -686,7 +686,7 @@ module.exports = function (app) {
 
     const _getAuthReqStatus = async (authType, token, timeoutMs) => {
         const tokenData = jwt.verify(token, config.session.publicKey, {algorithms: [config.session.algorithm]});
-        const loginFlowData = cryptoLib.privateDecrypt(tokenData.sessionDataEncrypted);
+        const loginFlowData = cryptoLib.decrypt(config.session.secret, tokenData.sessionDataEncrypted);
         let authLib, defaultErrorMessage;
         switch (authType) {
             case UserConnection.CONNECTION_IDS.smartid:
@@ -863,7 +863,7 @@ module.exports = function (app) {
         try {
             const sessionData = await mobileId.authenticate(pid, phoneNumber, null);
             sessionData.userId = userId;
-            const sessionDataEncrypted = {sessionDataEncrypted: cryptoLib.privateEncrypt(config.session.secret, sessionData)};
+            const sessionDataEncrypted = {sessionDataEncrypted: cryptoLib.encrypt(config.session.secret, sessionData)};
             const token = jwt.sign(sessionDataEncrypted, config.session.privateKey, {
                 expiresIn: '5m',
                 algorithm: config.session.algorithm
