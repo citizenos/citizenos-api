baef006ab5b091f3496a76d1dcf42e6b4c29f662
Victor <diver.vm@gmail.com>
2025-04-20 12:58:57 +0200

    #2205 (#458)
    * New Crowdin updates (#457)
    
    * New translations source.json (French)
    
    * New translations source.json (Spanish)
    
    * New translations source.json (Arabic)
    
    * New translations source.json (Belarusian)
    
    * New translations source.json (Bulgarian)
    
    * New translations source.json (Czech)
    
    * New translations source.json (German)
    
    * New translations source.json (Finnish)
    
    * New translations source.json (Japanese)
    
    * New translations source.json (Georgian)
    
    * New translations source.json (Lithuanian)
    
    * New translations source.json (Macedonian)
    
    * New translations source.json (Dutch)
    
    * New translations source.json (Polish)
    
    * New translations source.json (Russian)
    
    * New translations source.json (Slovak)
    
    * New translations source.json (Albanian)
    
    * New translations source.json (Ukrainian)
    
    * New translations source.json (Chinese Simplified)
    
    * New translations source.json (Portuguese, Brazilian)
    
    * New translations source.json (Indonesian)
    
    * New translations source.json (Estonian)
    
    * New translations source.json (Latvian)
    
    * New translations source.json (Hindi)
    
    * New translations source.json (English, United Kingdom)
    
    * New translations source.json (Burmese)
    
    * update delete reasons
    
    ---------
    
    Co-authored-by: tiblu <tiblu@users.noreply.github.com>

---
+++ db/migrations/20250419131407-alter-ideas-update-delete-reason-types.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.query(
+      `ALTER TYPE "enum_Ideas_deletedReasonType" ADD VALUE 'other' AFTER 'duplicate'`
+    );
+  },
+
+  down: async () => {
+    console.log('NO ROLLBACK FOR THIS MIGRATION.');
+    console.log('IF you want to revert this. You need to decide what to do with the data that has the new Report.type="other".');
+    console.log('Once the data has been cleaned up, that is, no "other" is used, you need to recreate the ENUM without the "other" because at this point PG does not support droping values from ENUM');
+  }
+};
